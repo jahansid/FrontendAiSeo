6445c4dae451f86d647e489d572d6b71c83908c0
jahan siddiqui <raj918833@gmail.com>
2024-08-15 00:08:24 +0530

    features section added

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { DotLottiePlayer } from "@dotlottie/react-player";
+import productImage from "@/assets/product-image.png";
+
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -26,5 +30,43 @@ const tabs = [
 ];
 
 export const Features = () => {
-  return <section>Features</section>;
+  return (
+    <section className=" py-20 md:py-24">
+      <div className=" container">
+        <h2 className=" text-5xl md:text-6xl tracking-tighter font-medium text-center">
+          Elevate your SEO efforts.
+        </h2>
+        <p className=" text-lg md:text-xl max-w-2xl mx-auto tracking-tight font-lg text-white/70 text-center mt-5">
+          From small startups to large enterprises, our AI-driven tool has
+          revolutionized the way businesses approach SEO.
+        </p>
+        <div className="  mt-10 flex flex-col lg:flex-row gap-3 ">
+          {tabs.map((tab) => (
+            <div
+              key={tab.title}
+              className=" border border-white/15 rounded-xl flex gap-2.5 p-2.5 items-center lg:flex-1"
+            >
+              <div className=" size-12 inline-flex justify-center items-center border border-white/15 rounded-lg">
+                <DotLottiePlayer src={tab.icon} className=" size-5" autoplay />
+              </div>
+              <div className="font-medium">{tab.title}</div>
+              {tab.isNew && (
+                <div className=" text-xs rounded-full px-2 py-0.5 text-black font-semibold bg-[#8c44ff]">
+                  new
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className=" border border-white/20 mt-3 p-2.5 rounded-xl">
+          <div
+            className=" aspect-video bg-cover border border-white/15 rounded-lg"
+            style={{
+              backgroundImage: `url(${productImage.src})`,
+            }}
+          ></div>
+        </div>
+      </div>
+    </section>
+  );
 };
